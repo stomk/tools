557552167d147454ae0df67e8b07da7217585ec5
Shingo Tomioka <s.tomioka.103@gmail.com>
2016-12-13 16:20:57 +0900

    Add faln.py

---
+++ faln.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description='Force align columns')
+parser.add_argument('-d', '--delimiter', dest='delim', default='\t', help='specify input delimiter')
+parser.add_argument('-a', '--align', choices=('l', 'r'), default='l', help="specify alignment, left 'l' or right 'r'")
+
+args = parser.parse_args()
+
+def calc_max_bytes(items):
+    return max(map(len, items))
+
+def extract_col_items(col_separated_lines, idx):
+    return map(lambda x: x[idx], col_separated_lines)
+
+lines = sys.stdin.readlines()
+col_separated_lines = map(lambda x: x.strip().split(args.delim), lines)
+num_col = len(col_separated_lines[0])
+
+max_bytes = []
+for i in range(num_col):
+    col_items = extract_col_items(col_separated_lines, i)
+    max_bytes.append(calc_max_bytes(col_items))
+
+if args.align == 'l':
+    fmt = '    '.join(map(lambda x: '%-' + str(x) + 's', max_bytes))
+else:
+    fmt = '    '.join(map(lambda x: '%' + str(x) + 's', max_bytes))
+
+for items in col_separated_lines:
+    print fmt % tuple(items)
